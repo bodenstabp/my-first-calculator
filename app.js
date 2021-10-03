3f887866bf6e8e6db0fa449a1f1aed1e19d03ffd
Paul Bodenstab <bodenstabp@gmail.com>
2021-10-02 23:57:15 -0400

    Fix for operations on screen bug and fraction calc framework added

--- app.js
+++ app.js
@@ -1,122 +1,169 @@
 const calculator = document.querySelector('.calculator');
-const screen = document.querySelector('.screen');
-const keySet = document.querySelectorAll('.calc-btn');
+const calcScreen = document.querySelector('.screen');
+const calcBtns = document.querySelectorAll('.calc-btn');
 const numberSet = document.querySelectorAll('.number');
 const operationSet = document.querySelectorAll('.operation');
+const fractionSet = document.querySelectorAll('.fraction');
 
-let itemOne, itemTwo, activeOperation, solution;
+let activeVariable, activeOperation, variableOne, variableTwo, result;
 
 const mathematics = {
   additionOperation: (a, b) => parseFloat(a + b),
   subtractionOperation: (a, b) => parseFloat(a - b),
   multiplicationOperation: (a, b) => parseFloat(a * b),
   divisionOperation: (a, b) => parseFloat(a / b),
-  percentageOperation: (a, b) => parseFloat((a / 100) * b),
-};
-
-const interface = {
-  // Memorize item
-  getItem: item => item,
-  // Display item
-  //   displayItem: display => (display.innerText = itemOne),
-  //   setDigit: (operation, varOne, varT, text) => {
-  //     if (operation) {
-  //       if (varTwo) {
-  //         varTwo += text;
-  //         screen.innerHTML = `<p>${item}</p>`;
-  //       } else {
-  //         varTwo = text;
-  //         screen.innerHTML = `<p>${item}</p>`;
-  //       }
-  //     } else {
-  //       if (itemOne) {
-  //         varOne += text;
-  //         screen.innerHTML = `<p>${item}</p>`;
-  //       }
-  //       varOne = text;
-  //       screen.innerHTML = `<p>${item}</p>`;
-  //     }
-  //   },
-  // Perform logic on item
-  performLogic: item => {
-    let itemText = item.innerText;
-    // Determine whether item is number or operation
-    // Put item in a variable if number
-    if (item.classList.contains('number')) {
-      if (activeOperation) {
-        if (itemTwo) {
-          itemTwo += itemText;
-          screen.innerHTML = `<p>${itemTwo}</p>`;
-        } else {
-          itemTwo = itemText;
-          screen.innerHTML = `<p>${itemTwo}</p>`;
-        }
-      } else {
-        activeOperation = undefined;
-        if (itemOne) {
-          itemOne += itemText;
-          screen.innerHTML = `<p>${itemOne}</p>`;
-        } else {
-          itemOne = itemText;
-          screen.innerHTML = `<p>${itemOne}</p>`;
-        }
-      }
-    } else if (item.classList.contains('operation')) {
-      if (itemText === '=') {
-        numericItemOne = parseFloat(itemOne);
-        numericItemTwo = parseFloat(itemTwo);
-        if (activeOperation === '+') {
-          solution = mathematics.additionOperation(
-            numericItemOne,
-            numericItemTwo
-          );
-        } else if (activeOperation === '-') {
-          solution = mathematics.subtractionOperation(
-            numericItemOne,
-            numericItemTwo
-          );
-        } else if (activeOperation === '÷') {
-          solution = mathematics.divisionOperation(
-            numericItemOne,
-            numericItemTwo
-          );
-        } else if (activeOperation === '*') {
-          solution = mathematics.multiplicationOperation(
-            numericItemOne,
-            numericItemTwo
-          );
-        } else if (activeOperation === '%') {
-          solution = mathematics.percentageOperation(
-            numericItemOne,
-            numericItemTwo
-          );
-        } else if (activeOperation.innerText == 'AC') {
-          itemOne = undefined;
-          itemTwo = undefined;
-          activeOperation = undefined;
-          solution = undefined;
-          screen.innerHTML = 'hi';
-        }
-        screen.innerHTML = `<p>${solution}</p>`;
-        // Reset variables when equal sign pressed
-        itemOne = undefined;
-        itemTwo = undefined;
-      }
-      return (activeOperation = itemText);
+  percentageOperation: (a, b) => parseFloat((b / 100) * a),
+  operationalLogic: () => {
+    switch (activeOperation) {
+      case '+':
+        result = mathematics.additionOperation(variableOne, variableTwo);
+        break;
+      case '-':
+        result = mathematics.subtractionOperation(variableOne, variableTwo);
+        break;
+      case '*':
+        result = mathematics.multiplicationOperation(variableOne, variableTwo);
+        break;
+      case '÷':
+        result = mathematics.divisionOperation(variableOne, variableTwo);
+        break;
+      case '%':
+        result = mathematics.percentageOperation(variableOne, variableTwo);
+        break;
+      default:
+        result = 'Error';
     }
   },
 };
 
-// Make buttons clickable
-keySet.forEach(key => {
-  key.addEventListener('click', e => {
-    // console.log(e.target.innerText == 'AC');
-    let item = interface.getItem(e.target);
-    interface.performLogic(item);
+const fractions = {
+  sixteenthDecimal: 0.0625,
+  eigthDecimal: 0.125,
+  quarterDecimal: 0.25,
+  halfDecimal: 0.5,
+  sixteenthFraction: '1/16',
+  eigthFraction: '1/8',
+  quarterFraction: '1/4',
+  halfFraction: '1/2',
+};
+
+const interactions = {
+  allClear: () => {
+    activeVariable = undefined;
+    activeOperation = undefined;
+    variableOne = undefined;
+    variableTwo = undefined;
+    result = undefined;
+  },
+  clear: () => {
+    activeVariable = undefined;
+  },
+  createNumericVariable: item => {
+    if (!activeVariable) {
+      return (activeVariable = item);
+    }
+    activeVariable += item;
+  },
+  setNumericVariable: () => {
+    activeOperation
+      ? (variableTwo = +activeVariable)
+      : (variableOne = +activeVariable);
+  },
+  setOperation: item => {
+    if (!item.classList.contains('special')) {
+      activeOperation = item.innerText;
+      interactions.clearActiveToggle();
+      item.classList.add('active');
+    }
+  },
+  displayActiveVariableOnScreen: () => {
+    if (activeOperation && variableOne) {
+      calcScreen.innerHTML = variableTwo;
+    } else if (activeOperation) {
+      calcScreen.innerHTML = variableOne;
+    } else {
+      calcScreen.innerHTML = activeVariable;
+    }
+  },
+  displayFractionKeyboard: () => {
+    numberSet.forEach(button => {
+      button.innerText = '';
+    });
+    const selectors = [fractions.sixteenthFraction, '1/8', '1/4', '1/2'];
+    for (let i = 0; i < selectors.length; i++) {
+      numberSet[i].innerText = selectors[i];
+      numberSet[i].classList.add('fraction');
+    }
+  },
+  displayStandardKeyboard: () => {
+    const selectors = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, '.'];
+    for (let i = 0; i < selectors.length; i++) {
+      numberSet[i].innerText = selectors[i];
+    }
+  },
+  clearActiveToggle: () => {
+    const activeToggles = document.querySelectorAll('.active');
+    activeToggles.forEach(item => {
+      item.classList.remove('active');
+    });
+  },
+  numericButtonControls: item => {
+    if (item.classList.contains('number')) {
+      interactions.setNumericVariable();
+      interactions.displayActiveVariableOnScreen();
+    }
+  },
+  specialButtonControls: item => {
+    if (item.classList.contains('special')) {
+      if (item.innerText === 'fr') {
+        if (item.classList.contains('active')) {
+          item.classList.remove('active');
+          interactions.displayStandardKeyboard();
+        } else {
+          item.classList.add('active');
+          interactions.displayFractionKeyboard();
+          console.log(item.innerText);
+        }
+      }
+      if (item.innerText === '=') {
+        mathematics.operationalLogic();
+        interactions.clearActiveToggle();
+        calcScreen.innerHTML = result;
+      }
+      if (item.innerText === 'AC') {
+        interactions.allClear();
+        calcScreen.innerHTML = ``;
+      }
+      if (item.innerText === 'C') {
+        interactions.clear();
+        calcScreen.innerHTML = ``;
+      }
+    }
+  },
+  operationButtonControls: item => {
+    if (item.classList.contains('operation')) {
+      activeVariable = undefined;
+    }
+    if (item.classList.contains('operation') && variableOne) {
+      if (result) {
+        variableOne = result;
+        variableTwo = undefined;
+      }
+      interactions.setOperation(item);
+      interactions.clear();
+    }
+  },
+};
+
+// Button Controls
+calcBtns.forEach(item => {
+  item.addEventListener('click', e => {
+    interactions.createNumericVariable(e.target.innerText);
+    interactions.numericButtonControls(e.target);
+    interactions.operationButtonControls(e.target);
+    interactions.specialButtonControls(e.target);
   });
 });
 
 // Bugs
-// operations should not create errors when pressed before numbers
-// C and AC buttons
-// Starting a new problem after getting solution
+// Need to be able to fresh start after pressing equal
